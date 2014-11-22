ab777a1d01235664c30bcd13a9d2fb7f63eaa4a6
Nathan Parry <nparry@gmail.com>
2014-11-22 15:41:49 -0500

    Enable specification of aws region

--- ec2/submit-spot-request.sh
+++ ec2/submit-spot-request.sh
@@ -6,10 +6,21 @@ USER_DATA_FILE="$(dirname $0)/ubuntu-userdata.sh"
 BILLSTORE_KEY=$1
 [ -z $BILLSTORE_KEY ] && echo "Please provide billstore key" && exit 1
 
+AWS_REGION=${2:-"us-east-1"}
+
+UBUNTU_OWNER="099720109477"
+AMI_NAME="ubuntu/images/ebs-ssd/ubuntu-trusty-14.04-amd64-server-20140927"
+AMI_ID=$(aws ec2 describe-images \
+  --region $AWS_REGION \
+  --owners $UBUNTU_OWNER \
+  --filters "Name=name,Values=$AMI_NAME" \
+  | grep '"ImageId":' | cut -d\" -f4)
+[ -z "$AMI_ID" ] && echo "Unable to location Ubuntu AMI" && exit 1
+
 USER_DATA=$(cat $USER_DATA_FILE | sed "s|BILLSTORE_KEY_VALUE|$BILLSTORE_KEY|" | base64)
 LAUNCH_SPEC=$(cat <<END
 {
-  "ImageId": "ami-98aa1cf0",
+  "ImageId": "$AMI_ID",
   "KeyName": "wintermute_aws",
   "SecurityGroups": ["mesos-spot-instances"],
   "UserData": "$USER_DATA",
@@ -19,6 +30,7 @@ END
 )
 
 aws ec2 request-spot-instances \
+  --region $AWS_REGION \
   --spot-price 0.01 \
   --instance-count 1 \
   --type persistent \
